bb8a9677a175586ffe7c1cd2c6c3c2dac357f6ab
Arpit Goyal <agoyal@calance.com>
2024-02-26 18:12:29 +0530

    restructured the workspace backup codebase entirely

--- app/repositories.py
+++ app/repositories.py
@@ -1,27 +1,8 @@
 import os
-import re
 
 
 def clone_repository(url, target):
     os.system("git clone {} {} > /dev/null 2>&1".format(url, target))
-
-
-def get_repo_name(repo_url):
-    """
-    handling wierd case when your git repo name has dots in it
-    plus another weird scenario when your repository name is
-    xyz.github.io.git
-    """
-
-    last_part = repo_url.split("/")[-1]
-    pattern = re.compile(r'(.*?)\.git$')
-
-    try:
-        repo_name = pattern.search(last_part).group(1)
-    except Exception:
-        repo_name = last_part
-
-    return repo_name.lower().replace("_", "-")
 
 
 def clone_repositories(config_data):
@@ -29,28 +10,28 @@ def clone_repositories(config_data):
     clones the repositories as specified in the config data
     """
 
-    folder_name = config_data["name"]
-    target_path = os.getenv("HOME") if not config_data["target_path"] else\
-        config_data["target_path"]
+    workspace_name = config_data["name"]
+    workspace_path = config_data["workspace_path"]
 
-    codes = config_data["codes"]
+    repositories = config_data["repositories"]
 
-    for source, apps in codes.items():
-        for app_type, repo_urls in apps.items():
-            for repo_url in repo_urls:
-                repo_name = get_repo_name(repo_url)
-                clone_path = os.path.join(
-                    target_path, folder_name, "codes",
-                    app_type, source, repo_name
-                )
+    for repository in repositories:
+        clone_name = repository["clone_name"]
+        clone_path = repository["clone_path"]
+        repo_url = repository["repo_url"]
 
-                if not os.path.exists(clone_path):
-                    print("Cloning {} repository {}".format(app_type,
-                                                            repo_name))
-                    os.system("mkdir -p {}".format(clone_path))
-                    clone_repository(repo_url, clone_path)
-                    continue
+        clone_location = os.path.join(
+            workspace_path, workspace_name, clone_path, clone_name
+        )
 
-                print("Skipping {} as path already present".format(
-                    repo_name
-                ))
+        if not os.path.exists(clone_location):
+            print("Cloning repository {} to path {}".format(
+                repo_url, clone_location
+            ))
+
+            clone_repository(repo_url, clone_location)
+            continue
+
+        print("Skipping {} as path already present".format(
+            repo_url
+        ))
---
+++ app/runners.py
@@ -0,0 +1,79 @@
+import os
+import json
+import shutil
+from .sources.aws import AwsSource
+from .sources.local import LocalSource
+from .repositories import clone_repositories
+
+
+def setup_from_template(workspace_name, workspace_path, config_folder):
+    """
+    creates the workspace in specific path based on the template
+    """
+
+    src_path = "structure"
+    dst_path = os.path.join(workspace_path, workspace_name)
+
+    if os.path.exists(dst_path):
+        print("Skipping creation as workspace {} in path {} already exists".format(
+            workspace_name, workspace_path)
+        )
+        return
+
+    print("Creating workspace {}".format(workspace_name))
+    shutil.copytree(src_path, dst_path)
+
+    with open("config_template.json") as f:
+        data = json.load(f)
+    
+    data["name"] = workspace_name
+    data["workspace_path"] = workspace_path
+
+    config_path = "{}/{}_config.json".format(config_folder, workspace_name)
+
+    with open(config_path, "w") as f:
+        json.dump(data, f)
+
+
+def backup_data(config_path):
+    """
+    calls the appropriate backup data class depending
+    on enabled sources
+    """
+
+    with open(config_path, 'r') as f:
+        config_data = json.load(f)
+
+    class_mapping = {
+        "s3": AwsSource(config_data),
+        "local": LocalSource(config_data)
+    }
+
+    enabled_sources = config_data["source_settings"]["enabled_sources"]
+
+    for src in enabled_sources:
+        print("Backing up data using {}".format(src))
+        class_mapping[src].backup_data()
+
+
+def restore_data(config_path):
+    """
+    calls the appropriate restore data class depending
+    on enabled sources
+    """
+
+    with open(config_path, 'r') as f:
+        config_data = json.load(f)
+
+    class_mapping = {
+        "s3": AwsSource(config_data),
+        "local": LocalSource(config_data)
+    }
+
+    enabled_sources = config_data["source_settings"]["enabled_sources"]
+
+    clone_repositories(config_data)
+
+    # for src in enabled_sources:
+    #     print("Backing up data using {}".format(src))
+    #     class_mapping[src].restore_data()
