3c8abf7ac43a097c227bca9ece087c66e5679019
Arpit Goyal <arpitgoyal.iitkgp@outlook.com>
2026-06-05 10:09:03 +0530

    feat: export GitSource from sources package

--- wbck/sources/__init__.py
+++ wbck/sources/__init__.py
@@ -1,10 +1,12 @@
 from .base import BaseSource
 from .aws import AwsSource
 from .local import LocalSource
+from .git import GitSource
 
 
 __all__ = [
     "BaseSource",
     "LocalSource",
-    "AwsSource"
+    "AwsSource",
+    "GitSource"
 ]
